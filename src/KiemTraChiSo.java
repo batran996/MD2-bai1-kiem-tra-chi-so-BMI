212ded2c01438e39a02633d63e8a0535189a9213
unknown <bazo20061996@gmail.com>
2022-07-25 09:17:19 +0700

    cap nhat

--- src/KiemTraChiSo.java
+++ src/KiemTraChiSo.java
@@ -4,23 +4,26 @@ public class KiemTraChiSo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double weight, height, bmi;
-        System.out.printf("Cùng tính BMI nhé :");
-        System.out.printf("Hãy nhập cân nặng của bạn");
+        System.out.println("Cùng tính BMI nhé :");
+        System.out.println("Hãy nhập cân nặng của bạn");
         weight = scanner.nextDouble();
-        System.out.printf("Hãy nhập chiều cao của bạn");
+        System.out.println("Hãy nhập chiều cao của bạn");
         height = scanner.nextDouble();
         bmi = weight / Math.pow(height, 2);
         if (bmi < 18.5)
             System.out.printf("BMI là " + bmi + "gầy");
 
-        if (bmi < 25)
+        if (18.5 == bmi || bmi < 25)
+
             System.out.printf("BMI là " + bmi + "bình thường");
 
-        if (bmi < 30)
+        if (25==bmi || bmi < 30)
+
             System.out.printf("BMI là " + bmi + "hơi mập");
 
         if (bmi >= 30)
-            System.out.printf("BMI là " + "quá mập");
+
+            System.out.printf("BMI là "+ bmi + "quá mập");
 
 
     }
